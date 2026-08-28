d5fa2c2dcbb6047950cc4b02d44871ad0d7f0857
Khaled Taha <khaled.taha.pro@gmail.com>
2026-08-28 14:43:20 +0300

    feat: add PDO database layer

---
+++ src/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+final class Database
+{
+    private PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=%s',
+            $config['host'],
+            $config['port'],
+            $config['name'],
+            $config['charset']
+        );
+
+        $this->pdo = new PDO($dsn, $config['user'], $config['pass'], [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+    }
+
+    public function pdo(): PDO
+    {
+        return $this->pdo;
+    }
+}
